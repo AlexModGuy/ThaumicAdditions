ab4e7f78ce31f5194c08b27615fa57e9c655a287
EndieDargon <endiedargon@gmail.com>
2018-05-17 08:03:06 +0300

    Added crystal water grow mechanic.

--- src/main/java/com/endie/thaumicadditions/blocks/BlockCrystalWater.java
+++ src/main/java/com/endie/thaumicadditions/blocks/BlockCrystalWater.java
@@ -2,17 +2,16 @@ package com.endie.thaumicadditions.blocks;
 
 import java.util.Random;
 
+import com.endie.thaumicadditions.init.BlocksTAR;
 import com.endie.thaumicadditions.init.FluidsTAR;
 import com.pengu.hammercore.api.iNoItemBlock;
 
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
+import thaumcraft.common.blocks.world.ore.BlockCrystal;
 
 public class BlockCrystalWater extends BlockFluidClassic implements iNoItemBlock
 {
@@ -24,18 +23,54 @@ public class BlockCrystalWater extends BlockFluidClassic implements iNoItemBlock
 	}
 	
 	@Override
-	public Vec3d getFlowVector(IBlockAccess world, BlockPos pos)
-	{
-		return super.getFlowVector(world, pos);
-	}
-	
-	@Override
 	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand)
 	{
 		super.updateTick(world, pos, state, rand);
-		if(world.rand.nextInt(4000) == 0)
+		
+		if(world.rand.nextInt(2000) == 0)
+			for(int x = -1; x <= 1; x += 2)
+				for(int z = -1; z <= 1; z += 2)
+				{
+					BlockPos ppos = pos.add(x, 1, z);
+					if(isCrystalGrowable(world, ppos))
+					{
+						growCrystal(world, ppos);
+						return;
+					}
+				}
+	}
+	
+	public static boolean isCrystalGrowable(World world, BlockPos pos)
+	{
+		if(world.getBlockState(pos).getBlock() instanceof BlockCrystal)
 		{
+			IBlockState state = world.getBlockState(pos);
+			int size = state.getValue(BlockCrystal.SIZE);
 			
+			for(int x = -1; x <= 1; x += 2)
+				for(int z = -1; z <= 1; z += 2)
+				{
+					BlockPos ppos = pos.add(x, -1, z);
+					IBlockState ibs = world.getBlockState(ppos);
+					if(ibs.getBlock() != BlocksTAR.CRYSTAL_WATER || ibs.getValue(LEVEL) > 0)
+						return false;
+				}
+			
+			return size < 3;
+		}
+		return false;
+	}
+	
+	public static void growCrystal(World world, BlockPos pos)
+	{
+		if(isCrystalGrowable(world, pos))
+		{
+			IBlockState state = world.getBlockState(pos);
+			int size = state.getValue(BlockCrystal.SIZE);
+			int sx = world.rand.nextInt(2) * 2 - 1;
+			int sz = world.rand.nextInt(2) * 2 - 1;
+			world.setBlockToAir(pos.add(sx, -1, sz));
+			world.setBlockState(pos, state.withProperty(BlockCrystal.SIZE, size + 1), 3);
 		}
 	}
 }
