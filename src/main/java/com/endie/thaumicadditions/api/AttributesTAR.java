5ba515f4d679bf6e2adf3a9ab5a410ca04b0a55e
EndieDargon <endiedargon@gmail.com>
2018-05-18 11:33:25 +0300

    Removed code from 1.12.2 branch

--- src/main/java/com/endie/thaumicadditions/api/AttributesTAR.java
+++
@@ -1,9 +0,0 @@
-package com.endie.thaumicadditions.api;
-
-import net.minecraft.entity.ai.attributes.IAttribute;
-import net.minecraft.entity.ai.attributes.RangedAttribute;
-
-public class AttributesTAR
-{
-	public static final IAttribute SOUND_SENSIVITY = new RangedAttribute(null, "generic.soundSensivity", 1, 0, 10).setDescription("Sound Sensivity").setShouldWatch(true);
-}
--- ↓ Setup.bat
+++
@@ -1,2 +0,0 @@
-gradlew setupDecompWorkspace eclipse
-pause
